16511b10723c04f4a7baa3d64901c16cdc2dbb8b
selenaxiao-nist <selena.xiao@nist.gov>
2025-06-06 17:30:46 -0400

    ac-2c

---
+++ ops/assess-ops/assessments/ac-2c.py
@@ -0,0 +1,25 @@
+# ac-2c - parse SSP and determine whether all users have a NIST valid email
+
+from utils import *
+
+print_current_control(__file__)
+
+expr_emails = f"//oscal:party/oscal:email-address/text()"
+
+get_emails = run_ssp_expr(expr_emails)
+
+print("SSP description:\n")
+print(get_ssp_statement_description("c") + "\n")
+print("SAP description:\n")
+print(get_sap_task_description("c") + "\n")
+
+print(f"{get_emails=}")
+
+get_valid_emails = []
+for email in get_emails:
+    email = str(email)
+    if email.endswith('@nist.gov'):
+        get_valid_emails.append(email)
+
+result = "Pass" if len(get_emails) == len(get_valid_emails) else "Fail"
+print("Result: " + result)
