aaf4718879580799475814304f8c35c088425ec6
selenaxiao-nist <selena.xiao@nist.gov>
2025-06-12 16:15:53 -0400

    ac-2e in progress

--- ops/assess-ops/assessments/ac-2e.py
+++ ops/assess-ops/assessments/ac-2e.py
@@ -16,5 +16,19 @@ def get_approved_requests(organization: str, repo: str) -> list:
 
     return closed_issues
 
-def get_approvers(closed_issues: list) -> list:
-    return []
+def get_approver(closed_issues: list, organization: str, repo: str) -> str:
+    url = f"https://api.github.com/repos/{organization}/{repo}/issues?state=closed"
+    headers = {"Accept": "application/vnd.github+json"}
+    # assumes public repo
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        print(f"Failure code when getting approver of request: {response.status_code}")
+        exit(1)
+
+
+def get_approvers(closed_issues: list, organization: str, repo: str) -> list:
+    approvers = []
+    for issue in closed_issues:
+        approvers.append(get_approver(issue, organization, repo))
+    
+    return list(set(approvers))
